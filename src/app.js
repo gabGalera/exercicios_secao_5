5732b6157226bee7f866b84eb7cd9a407cb80ff1
thayscosta3 <thayscosta3@gmail.com>
2022-10-05 16:58:50 -0300

    Altera isPassengerExists para doesPassengerExist

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const DRIVER_ON_THE_WAY = 2;
 const TRAVEL_IN_PROGRESS = 3;
 const TRAVEL_FINISHED = 4;
 
-const isPassengerExists = async (passengerId) => {
+const doesPassengerExist = async (passengerId) => {
   const [[passenger]] = await connection.execute(
     'SELECT * FROM passengers WHERE id = ?',
     [passengerId],
@@ -33,7 +33,7 @@ app.post('/passengers/:passengerId/request/travel', async (req, res) => {
   const { passengerId } = req.params;
   const { startingAddress, endingAddress, waypoints } = req.body;
 
-  if (await isPassengerExists(passengerId)) {
+  if (await doesPassengerExist(passengerId)) {
     const [resultTravel] = await connection.execute(
       `INSERT INTO travels 
           (passenger_id, starting_address, ending_address) VALUE (?, ?, ?)`,
