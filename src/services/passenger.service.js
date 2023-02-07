91837ac74b0025c6b983810c29aaa4cb00eb4058
Gabriel Galera <gabgalera@hotmail.com>
2023-02-07 19:28:31 -0300

    Implementa o serviço findById e seus testes

--- src/services/passenger.service.js
+++ src/services/passenger.service.js
@@ -1,10 +1,22 @@
 const { passengerModel } = require('../models');
+const schema = require('./validations/validationsInputValues');
 
 const findAll = async () => {
   const passengers = await passengerModel.findAll();
   return { type: null, message: passengers };
 };
 
+const findById = async (passengerId) => {
+  const error = schema.validateId(passengerId);
+  if (error.type) return error;
+
+  const passenger = await passengerModel.findById(passengerId);
+  if (!passenger) return { type: 'PASSENGER_NOT_FOUND', message: 'Passenger not found' };
+
+  return { type: null, message: passenger };
+};
+
 module.exports = {
   findAll,
+  findById,
 };
--- tests/unit/services/passenger.service.test.js
+++ tests/unit/services/passenger.service.test.js
@@ -19,6 +19,43 @@ describe('Verificando service pessoa passageira', function () {
       expect(result.message).to.deep.equal(allPassengers);
     });
   });
+
+  describe('busca de uma pessoa passageira', function () {
+    it('retorna um erro caso receba um ID inválido', async function () {
+      // arrange: Especificamente nesse it não temos um arranjo pois nesse fluxo o model não é chamado!
+
+      // act
+      const result = await passengerService.findById('a');
+      
+      // assert
+      expect(result.type).to.equal('INVALID_VALUE');
+      expect(result.message).to.equal('"id" must be a number');
+    });
+
+    it('retorna um erro caso a pessoa passageira não existe', async function () {
+      // arrange
+      sinon.stub(passengerModel, 'findById').resolves(undefined);
+    
+      // act
+      const result = await passengerService.findById(1);
+      
+      // assert
+      expect(result.type).to.equal('PASSENGER_NOT_FOUND');
+      expect(result.message).to.equal('Passenger not found');
+    });
+    
+    it('retorna a pessoa passageira caso ID existente', async function () {
+      // arrange
+      sinon.stub(passengerModel, 'findById').resolves(allPassengers[0]);
+      
+      // act
+      const result = await passengerService.findById(1);
+
+      // assert
+      expect(result.type).to.equal(null);
+      expect(result.message).to.deep.equal(allPassengers[0]);
+    });
+  });
   
   afterEach(function () {
     sinon.restore();
