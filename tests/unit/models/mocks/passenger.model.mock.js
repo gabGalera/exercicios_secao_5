1ad7fd8507345982eba35ae32ddeaa24ae03e26a
Gabriel Galera <gabgalera@hotmail.com>
2023-02-07 15:18:28 -0300

    Implementa o teste da função findAll

---
+++ tests/unit/models/mocks/passenger.model.mock.js
@@ -0,0 +1,37 @@
+// tests/unit/models/mocks/passenger.model.mock.js
+const passengers = [
+  {
+    id: 1,
+    name: 'Doriana Quintal',
+    email: 'doriana.quintal@acme.com',
+    phone: '(49) 3882-3117',
+  },
+  {
+    id: 2,
+    name: 'Leo Sampaio',
+    email: 'leo.sampaio@acme.com',
+    phone: '(66) 3692-7793',
+  },
+  {
+    id: 3,
+    name: 'Anabela Monteiro',
+    email: 'anabela.monteiro@acme.com',
+    phone: '(49) 2134-2152',
+  },
+  {
+    id: 4,
+    name: 'Estêvão Paranhos',
+    email: 'estevao.paranhos@acme.com',
+    phone: '(82) 2166-2413',
+  },
+  {
+    id: 5,
+    name: 'Mateo Vidigal',
+    email: 'mateo.vidigal@acme.com',
+    phone: '(51) 2303-7355',
+  },
+];
+
+module.exports = {
+  passengers,
+};
---
+++ tests/unit/models/passenger.model.test.js
@@ -0,0 +1,22 @@
+// tests/unit/models/passenger.model.test.js
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { passengerModel } = require('../../../src/models');
+
+const connection = require('../../../src/models/connection');
+const { passengers } = require('./mocks/passenger.model.mock');
+
+describe('Testes de unidade do model de pessoas passageiras', function () {
+  it('Recuperando a lista de pessoas passageiras', async function () {
+    // Arrange
+    sinon.stub(connection, 'execute').resolves([passengers]);
+    // Act
+    const result = await passengerModel.findAll();
+    // Assert
+    expect(result).to.be.deep.equal(passengers);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
