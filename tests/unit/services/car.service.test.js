1587f6c12f066fd6ed79535fed16182e0429d8cb
Gabriel Galera <gabgalera@hotmail.com>
2023-02-10 12:02:55 -0300

    Adiciona testes para os casos inválidos

--- tests/unit/services/car.service.test.js
+++ tests/unit/services/car.service.test.js
@@ -7,7 +7,46 @@ const { carModel } = require('../../../src/models');
 const { model, color, licensePlate, id } = newCarMock.message;
 
 describe('listagem de carros', function () {
-  it('Cria um novo carro', async function () {
+  it('Cria um novo carro com modelo inválido', async function () {
+    // Arrange
+    sinon.stub(carModel, 'insert').resolves(id);
+    sinon.stub(carModel, 'findById').resolves(newCarMock.message);
+
+    // Act
+    const result = await carService.createCar({ model: 'D', color, licensePlate });
+
+    // Assert
+    expect(result.message).to.be.equal('"model" length must be at least 3 characters long');
+    expect(result.type).to.be.equal('INVALID_VALUE');
+  });
+
+  it('Cria um novo carro com color inválido', async function () {
+    // Arrange
+    sinon.stub(carModel, 'insert').resolves(id);
+    sinon.stub(carModel, 'findById').resolves(newCarMock.message);
+
+    // Act
+    const result = await carService.createCar({ model, color: 'D', licensePlate });
+
+    // Assert
+    expect(result.message).to.be.equal('"color" length must be at least 2 characters long');
+    expect(result.type).to.be.equal('INVALID_VALUE');
+  });
+
+  it('Cria um novo carro com licensePlate inválido', async function () {
+    // Arrange
+    sinon.stub(carModel, 'insert').resolves(id);
+    sinon.stub(carModel, 'findById').resolves(newCarMock.message);
+
+    // Act
+    const result = await carService.createCar({ model, color, licensePlate: 'D' });
+
+    // Assert
+    expect(result.message).to.be.equal('"licensePlate" length must be at least 7 characters long');
+    expect(result.type).to.be.equal('INVALID_VALUE');
+  });
+
+  it('Cria um novo carro com sucesso', async function () {
     // Arrange
     sinon.stub(carModel, 'insert').resolves(id);
     sinon.stub(carModel, 'findById').resolves(newCarMock.message);
@@ -17,5 +56,9 @@ describe('listagem de carros', function () {
 
     // Assert
     expect(result.message).to.be.deep.equal(newCarMock.message);
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
